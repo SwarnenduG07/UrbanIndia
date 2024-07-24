620c51a4bd4bbef1e61dba0426ac46fa66d2266a
Sunny <swarnendughosh05@gmail.com>
2024-07-25 01:28:07 +0530

    shadcn init and env dleted

---
+++ Readme.md
@@ -0,0 +1,12 @@
+# Urban India 
+
+## Local dev 
+
+```bash
+git clone 
+cd backend
+npm i
+cd ../frontend
+npm i 
+npm run dev
+```
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,15 @@
 import { useState } from 'react'
 import './App.css'
 import TypewriterComponent from 'typewriter-effect'
+import { Button } from './components/ui/button'
+import { Card } from './components/ui/card'
 
 function App() {
 
   return (
-    <>
+  
       <div className='w-full items-center flex flex-col justify-center h-screen font-bold text-6xl bg-gradient-to-r from-cyan-500 to-blue-500'>
-
+   
       <TypewriterComponent 
               options={{
                 strings: [
@@ -17,10 +19,20 @@ function App() {
                 loop: true,
               }}
              />
+     
+     
+             <div>
+                 <Button variant = "premium" onClick={() => {
+                      alert("Fuck Me more I am getting fun o yeaa!")
+                 }}>
+                   FUCK ROBIN VERY HARD
+                 </Button>
+             </div>
+             
          
       </div>
         
-    </>
+    
   )
 }
 
